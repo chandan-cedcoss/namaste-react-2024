db1c0834cbe6a45e4d7989d0b0da32c2ae68b7fa
Chandan Pandey <csp7089@gmail.com>
2024-08-06 02:51:43 +0530

    react fundamentals

--- App.js
+++ App.js
@@ -1,22 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am a h1 tag"),
-    React.createElement("h2", {}, "I am a h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am a h1 tag"),
-    React.createElement("h2", {}, "I am a h2 tag"),
-  ]),
-]);
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World with React..!!!"
-);
-console.log(parent); // return and javascript object.
+//React Element:
+const JsxHeading = () => (<h1 className="heading" tabIndex={1}>Namaste React Using JSX</h1>);
+
+//React Functional Component with component composition:
+const HeadingComponent = () => (<div id="heading"><JsxHeading /><h1>This is the first namsate react functional component..!!</h1></div>);
+
+
+
+console.log(HeadingComponent); // return and javascript object.
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); // render function converts heading object into a html tag.
+root.render(<HeadingComponent />); // render function converts heading object into a html tag.
